d52229a1dfae3ef68ce2c22ae756e0b048476bca
Pratibha Chaurasiya <115644671+emerging-pc@users.noreply.github.com>
2023-10-06 18:19:41 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 //Task 1- BUG TRACKER
+A bug tracking system is an application that lets you keep track of bugs (and often suggestions) for your software project in a database.
 
 #include <conio.h>
 #include <stdio.h>
